1ae47113d7bf59b5e59cc96a67776e848ec91eb9
Radomiej <radek1993-91@o2.pl>
2020-04-21 10:59:06 +0000

    up

--- GitLab-small-enhanced.user.js
+++ GitLab-small-enhanced.user.js
@@ -9,7 +9,7 @@
 // @homepageURL https://github.com/Radomiej/tampermonkey-scripts
 // @match http://gitlab.itti.com.pl/*/*/pipelines
 // @require http://code.jquery.com/jquery-3.4.1.min.js
-// @icon https://github.com/Radomiej/tampermonkey-scripts/blob/fbdcb5d2dcb8ce3c7ddc45f954b86ebd75e0c086/gitlab-icon-1-color-black-rgb.png
+// @icon https://github.com/Radomiej/tampermonkey-scripts/raw/1965646c6a9524d77a850e1af240ccead5936d95/gitlab-icon-1-color-black-rgb.png
 // @grant GM_addStyle
 // @run-at  document-idle
 // ==/UserScript==
